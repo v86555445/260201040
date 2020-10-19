3a9de051ce800a17c0ee1bbd43bc0296e4193d7f
v86555445 <furkanyavuz0926@gmail.com>
2020-10-19 09:43:49 +0000

    1

---
+++ lab2/example5.py
@@ -0,0 +1,4 @@
+length1=float(input("length of perpendicular edges 1:"))
+length2=float(input("length of perpendicular edges 2:"))
+hypotenuse=(length1**2+length2**2)**0.5
+print("hypotenuse :",hypotenuse)
