8a394dd7e7f57a91e39c8d631860268649dc691c
v86555445 <furkanyavuz0926@gmail.com>
2020-10-26 09:11:09 +0000

    1

--- lab3/example4.py
+++ lab3/example4.py
@@ -1,7 +1,9 @@
 age=int(input("enter your age :"))
-if age<6 or age>60:
+if 0<=age<6 or age>60:
   print("0 TL")
 elif 6<=age<=18:
   print("1.5 TL")
+elif age<0:
+  print(" Please write a appropriate age")
 else:
   print("3 TL")
