582573d22450abb5fc6afb7fdfb583ba58b460cc
v86555445 <furkanyavuz0926@gmail.com>
2020-10-26 08:42:56 +0000

    1

---
+++ lab3/example2.py
@@ -0,0 +1,10 @@
+a=float(input("enter a three num1 :"))
+b=float(input("enter a three num2 :"))
+c=float(input("enter a three num3 :"))
+if a<b and a<c:
+  print(a)
+elif b<a and b<c:
+  print(b)
+else:
+  print(c)
+
