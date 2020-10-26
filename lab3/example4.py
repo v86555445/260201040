7da73c1f494ccf0d1a1b95f9499cc359f88d2378
v86555445 <furkanyavuz0926@gmail.com>
2020-10-26 09:16:00 +0000

    1

--- lab3/example4.py
+++ lab3/example4.py
@@ -1,9 +1,10 @@
 age=int(input("enter your age :"))
+price=3
 if 0<=age<6 or age>60:
-  print("0 TL")
+  print(price*0, "TL")
 elif 6<=age<=18:
-  print("1.5 TL")
+  print(price*0.5,"TL")
 elif age<0:
   print(" Please write a appropriate age")
 else:
-  print("3 TL")
+  print(price,"TL")
