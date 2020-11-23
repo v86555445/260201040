8c8d0bd16e87db2ee39c8ca54993d906d58a1207
v86555445 <furkanyavuz0926@gmail.com>
2020-11-23 09:39:38 +0000

    1

--- lab6/example2.py
+++ lab6/example2.py
@@ -3,14 +3,10 @@ s=0
 al=[]
 average_grades=[]
 while num_of_students>s:
-  o=[]
   md1=float(input("input midterm 1 :"))
   md2=float(input("input midterm 2 :"))
   fnl=float(input("input final :"))
-  o.append(md1)
-  o.append(md2)
-  o.append(fnl)
-  al.append(o)
+  al.append([md1,md2,fnl])
   average=md1*30/100+md2*30/100+fnl*40/100
   average_grades.append(average)
   s+=1
