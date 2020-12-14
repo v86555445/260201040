20748e87bb6100a2ab6c3a3f9d0076aaccb66fe0
v86555445 <furkanyavuz0926@gmail.com>
2020-12-14 10:14:30 +0000

    1

---
+++ lab8/example5.py
@@ -0,0 +1,24 @@
+def password_checker(p):
+  if len(p)<8 or " " in p:
+    return 0
+  else:
+    level=0
+    alpha=False
+    numeric=False
+    Specialchar=False
+    for i in p:
+      if i.isalpha():
+        alpha=True
+      elif i.isnumeric():
+        numbers=True
+      else:
+        Specialchar=True
+    if alpha==True:
+      level+=1
+    if numeric==True:
+      level+=1
+    if Specialchar==True:
+      level+=1
+    return level
+pw=(input("Enter password:"))
+print("Security level :",password_checker(pw))
