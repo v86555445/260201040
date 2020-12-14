e837e895c3384ffff534ed306400eab75b3b7049
v86555445 <furkanyavuz0926@gmail.com>
2020-12-14 09:09:52 +0000

    1

---
+++ lab7/example3.py
@@ -0,0 +1,5 @@
+employees=dict()
+for i in range(5):
+  name=input("enter name of employee")
+  salary=float(input("enter a salary of the employee"))
+  employees[name]=salary
---
+++ lab8/example3.py
@@ -0,0 +1,17 @@
+import random
+def get_rand_list(b,e,N):
+  l=[]
+  for i in range(N):
+    a=random.randint(b,e)
+    l.append(a)
+  return l
+def get_overlap(a,b):
+  intersection=set()
+  for i in a:
+    if i in b:
+      intersection.add(i)
+  return list(intersection)
+a=get_rand_list(0,10,5)
+b=get_rand_list(0,10,5)
+
+print(get_overlap(a,b))
